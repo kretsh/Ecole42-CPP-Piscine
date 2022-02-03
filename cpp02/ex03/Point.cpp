623331111db85b02ed15e397fad536b156450a57
Clorinda Owen <cowen@oa-b1.msk.21-school.ru>
2022-02-03 16:06:38 +0300

    Fixed operator=

--- cpp02/ex03/Point.cpp
+++ cpp02/ex03/Point.cpp
@@ -6,7 +6,7 @@
 /*   By: cowen <cowen@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/02 19:22:20 by cowen             #+#    #+#             */
-/*   Updated: 2022/02/02 21:38:43 by cowen            ###   ########.fr       */
+/*   Updated: 2022/02/03 14:57:51 by cowen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,7 @@ Point::Point(const Point & other) : _x(other._x), _y(other._y){
 Point	Point::operator=(const Point & rhs){
 	if (this != &rhs){
 		Point temp(rhs._x.getRawBits(), rhs._y.getRawBits());
+		this->~Point();
 		return temp;
 	}
 	return *this;
