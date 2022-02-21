726f7d992b785a3693082f7331fb5482e5837ea8
Clorinda Owen <cowen@oa-b1.msk.21-school.ru>
2022-02-21 20:55:42 +0300

    fixes

--- cpp08/ex01/Span.hpp
+++ cpp08/ex01/Span.hpp
@@ -6,9 +6,12 @@
 /*   By: cowen <cowen@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/20 20:42:31 by cowen             #+#    #+#             */
-/*   Updated: 2022/02/21 18:08:10 by cowen            ###   ########.fr       */
+/*   Updated: 2022/02/21 20:54:45 by cowen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef SPAN_HPP
+#define SPAN_HPP
 
 #include <iostream>
 #include <vector>
@@ -37,3 +40,4 @@ class Span{
 		int longestSpan();
 		~Span();
 };
+#endif
